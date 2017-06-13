8c346038302e454d5b513ea3f7f7925acac0f797
Adrien Carteron <adcarter@localhost.localdomain>
2017-06-13 17:09:09 +0200

    [ServicesKabare:Sakilol] Add global summary and blue visual for battery information

--- libs/js/summary.js
+++ libs/js/summary.js
@@ -5,29 +5,29 @@ function loadWhatTheShit(){
     getSomething(displaySummary,"/summary/"+date);
 }
 function getPrev(){
-    //var d=strToDate(document.getElementById("date_yesterday").innerHTML);
     var date=formatDateMinusOne(document.getElementById("date_yesterday").innerHTML);
     document.getElementById("date_yesterday").innerHTML = date;
     getSomething(displaySummary,"/summary/"+date);
 }
 function getNext(){
-    //var d=strToDate(document.getElementById("date_yesterday").innerHTML);
     var date=formatDatePlusOne(document.getElementById("date_yesterday").innerHTML);
     document.getElementById("date_yesterday").innerHTML = date;
     getSomething(displaySummary,"/summary/"+date);
 }
 function displaySummary(summary){
-    date_y=summary.date;
-    document.getElementById("date_yesterday").innerHTML = date_y;
+    var date_y=summary.date;
     var prio_list=summary["priorities"];
     var results=summary.results;
     var container="<div class=\"container\"><div class=\"panel-group\" id=\"accordion\">";
-   
+    var all_prios=[];
+    for( var k = 0 ; k < prio_list.length ; k ++){
+	var one_prio={name:"",number:0};
+	one_prio.name=prio_list[k];
+	all_prios.push(one_prio);
+    }	
     for(var i = 0 ; i < results.length ; i ++) {
 	var test_res=results[i];
 	var begin_panel="<div class=\"panel panel-default\"><div class=\"panel-heading\"><h4 class=\"panel-title\"><a data-toggle=\"collapse\" data-parent=\"#accordion\" href=\"#collapse"+i+"\">";
-
-	//var begin_panel="<div class=\"panel panel-default\"><div class=\"panel-heading\"><h4 class=\"panel-title\"><a data-toggle=\"collapse\" href=\"#collapse"+i+"\">";
 	
 	var end_panel_header="</a></h4></div><div id=\"collapse"+i+"\" class=\"panel-collapse collapse\"><div class=\"panel-body\">";
 	var end_panel="</div></div></div>";
@@ -35,6 +35,7 @@ function displaySummary(summary){
 	var user_prio_enabled_lits=test_res.priorities;
 	var user_rules_list=[];
 	var prio_numbers=[];
+	var only_battery=true;
 	for(var j=0;j<user_prio_enabled_lits.length;j++){
 	    var user_prio=user_prio_enabled_lits[j]
 	    var one_prio={name:"",number:0};
@@ -45,6 +46,8 @@ function displaySummary(summary){
 		var test1_2=rule_for_prio[k];
 		var test2=test[test1_2];
 		if(test2!==undefined){
+		    if(test1_2!=='batteryAlert'&&test1_2!=='batteryWarning')
+			only_battery=false;
 		    var rule_name="<div class=\"panel panel-default\"><div class=\"panel-heading\"><span class=\"label label-default\">"+one_prio.name+"</span> "+test1_2+"</div>  <div class=\"panel-body\">";
 		    var streams_array=test2.streams;
 		    var rule_panel_body="<ul>";
@@ -62,85 +65,51 @@ function displaySummary(summary){
 		    rule_name+=rule_panel_body;
 		    body+=rule_name;
 		    var test3=test2.number;	
-		    //console.log(test3);
 		    one_prio.number+=Number(test3);
 		}
 	    }
 	    user_rules_list.push(results[i][user_prio_enabled_lits[j]]);
 	    prio_numbers.push(one_prio);
 	}
-	//console.log(JSON.stringify(prio_numbers));
 	var bad_tings="";
 	for( var k = 0 ; k < prio_list.length ; k ++){
-	    //bad_tings+=prio_list[k]+": ";
+	   
 	    var found=0;
 	    var number_f="";
 	    for(var j=0;j<prio_numbers.length&&found===0;j++){	
 		if(prio_numbers[j].name===prio_list[k]){
 		    number_f=prio_numbers[j].number;
+		    all_prios[k].number+=number_f;
 		    found=1;
 		}
 	    }
 	    if(found===0){
 		bad_tings+="<span class=\"label label-primary\">"+prio_list[k]+"</span>";
 	    }else{
-		bad_tings+="<span class=\"label label-warning\">"+prio_list[k]+": "+number_f+"</span>";
-	    }
-	    
-	}
-	
-	var header="<h4>"+results[i].user.replace("_","")+"</h4> "+bad_tings;//JSON.stringify(prio_list)+" "+JSON.stringify(prio_numbers);
-	//body=JSON.stringify(user_rules_list);
-	container+=begin_panel+header+end_panel_header+body+end_panel;
-	
+		if(only_battery===false)
+		    bad_tings+="<span class=\"label label-danger\">"+prio_list[k]+": "+number_f+"</span>";
+		else
+		    bad_tings+="<span class=\"label label-info\">"+prio_list[k]+": "+number_f+"</span>";
+	    }	    
+	}	
+	var header="<h4>"+results[i].user.replace("_","")+"</h4> "+bad_tings;
+	container+=begin_panel+header+end_panel_header+body+end_panel;	
     }
+
+    
     var end_container="</div>";
-    document.getElementById("demo").innerHTML = container;
+    
+    var main_header="<div class=\"panel panel-default\"><div class=\"panel-body\">";
+    for( var i = 0 ; i < all_prios.length ; i ++){
+	var name=all_prios[i].name;
+	var number=all_prios[i].number;
+	if(number===0)
+	    main_header+="<span class=\"label label-primary\">"+name+"</span>";
+	else
+	    main_header+="<span class=\"label label-danger\">"+name+": "+number+"</span>";
+    }
+    main_header+="</div></div>";
+    document.getElementById("date_yesterday").innerHTML = date_y;
+
+    document.getElementById("demo").innerHTML = main_header+container;
 }
-
-
-/*
-
-<div class="container">
-  <p><strong>Note:</strong> The <strong>data-parent</strong> attribute makes sure that all collapsible elements under the specified parent will be closed when one of the collapsible item is shown.</p>
-  <div class="panel-group" id="accordion">
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h4 class="panel-title">
-          <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">Collapsible Group 1</a>
-        </h4>
-      </div>
-      <div id="collapse1" class="panel-collapse collapse in">
-        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
-      </div>
-    </div>
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h4 class="panel-title">
-          <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">Collapsible Group 2</a>
-        </h4>
-      </div>
-      <div id="collapse2" class="panel-collapse collapse">
-        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
-      </div>
-    </div>
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h4 class="panel-title">
-          <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">Collapsible Group 3</a>
-        </h4>
-      </div>
-      <div id="collapse3" class="panel-collapse collapse">
-        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
-      </div>
-    </div>
-  </div> 
-</div>
-
-*/
